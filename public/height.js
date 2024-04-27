c22e2a9ed39c22ea5fd081dad5f2817878fee487
Max Globa <onyx18121990@gmail.com>
2024-04-27 20:19:14 +0300

    changed svg

--- public/height.js
+++
@@ -1,9 +0,0 @@
-const setHeight = () => {
-  let vh = window.innerHeight * 0.01;
-  let vw = window.innerWidth;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
-
-setHeight()
-
-window.addEventListener('resize', () => setHeight())
